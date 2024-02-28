8b4827b309bcf82fb546d837ed3a4aec61fcdfb9
sycocracks <105314103+sycocracks@users.noreply.github.com>
2024-02-28 17:32:18 +0100

    adjustments

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -55,12 +55,12 @@ const Footer = () => {
                         <li className="hLinks">
                             <Link to= "/terms"> Terms of Delivery</Link>
                         </li>
-                        <li className="hLinks">
+                        {/* <li className="hLinks">
                             <Link to= "/conditions"> Terms and Condition</Link>
-                        </li>
-                        <li className="hLinks">
+                        </li> */}
+                        {/* <li className="hLinks">
                             <Link to= "/returns">Returns</Link>
-                        </li>
+                        </li> */}
                         <li className="hLinks">
                             <Link to= "/policy">Privacy Policy</Link>
                         </li>
@@ -68,7 +68,7 @@ const Footer = () => {
                 </div>
                 <div>
                     <h2>About Us</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo expedita blanditiis exercitationem reprehenderit id. Animi expedita praesentium aspernatur nihil?</p>
+                    <p>Located in the vibrant heart of Orlando, Florida, olly is your ultimate destination for all things trendy and TikTok-inspired. We pride ourselves on curating the hottest selection of products that are sure to elevate your social media game and keep you ahead of the curve.</p>
                 </div>
                 <div className="newsletter">
                     <h2>Newsletter</h2>
